7e14979207a389ff340c1ae051677516777a102a
AndreiBrilliantov <andrewbrull@gmail.com>
2023-06-13 15:37:06 +0300

    add hw_1/Task006 and hw_1/Task008

---
+++ hw_1/Task_008/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+
+Задача 8: Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+
+*/
+
+Console.WriteLine("Input your number =>");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write(number + " -> ");
+for (int i = 0; i <= number; i += 2)
+{
+    if(i % 2 == 0 && i != 0) {
+        Console.Write(i);
+        if(i != number) 
+            Console.Write(", ");
+    }
+}
