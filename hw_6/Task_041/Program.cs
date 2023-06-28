4f84720348523544d301492b04de922a124bd0ad
AndreiBrilliantov <andrewbrull@gmail.com>
2023-06-28 12:24:02 +0300

    add hw_6 with Task_041 _043

---
+++ hw_6/Task_041/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+
+*/
+void OutNumber(int[] number, int number2) {
+    Console.WriteLine($"{string.Join(", ", number)} -> {number2}");
+}
+
+int InputNumber(string msg = "Input your number =>") {
+    Console.WriteLine(msg);
+    int number;
+    while (true)
+    {
+        string temp = Console.ReadLine();
+        if(int.TryParse(temp, out number)){
+            return number;
+        }
+        Console.Write($"This number \"{temp}\" is not correct. Try again =>");
+    }
+}
+
+int[] InputArray(int size){
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = InputNumber($"Input {i+1}/{size} number");
+    }
+    return arr;
+}
+
+bool IsGreaterThanZero(int num){
+    return num > 0 ? true : false;
+}
+
+int NumGreaterThanZero(int[] arr){
+    int count = 0;
+    foreach (int el in arr)
+    {
+        count += IsGreaterThanZero(el) ? 1 : 0;
+    }
+    return count;
+}
+
+int size = InputNumber("Input array size");
+int[] arr = InputArray(size);
+OutNumber(arr, NumGreaterThanZero(arr));
---
+++ hw_6/Task_043/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+
+Задача 43: Напишите программу, которая найдёт точку пересечения 
+двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+*/
+void OutNumber((int k1, int b1, int k2, int b2) lineKoef, (double pointX, double pointY) p) {
+    Console.WriteLine($"b1 = {lineKoef.b1}, k1 = {lineKoef.k1}, b2 = {lineKoef.b2},  k2 = {lineKoef.k2} -> ({p.pointX}; {p.pointY})");
+}
+int InputNumber(string msg = "Input your number =>") {
+    Console.WriteLine(msg);
+    int number;
+    while (true)
+    {
+        string temp = Console.ReadLine();
+        if(int.TryParse(temp, out number)){
+            return number;
+        }
+        Console.Write($"This number \"{temp}\" is not correct. Try again =>");
+    }
+}
+(int k1, int b1, int k2, int b2) InputKoefLine() {
+    return (
+        k1:InputNumber("Input k1 for first line"),
+        b1:InputNumber("Input b1 for first line"),
+        k2:InputNumber("Input k2 for second line"),
+        b2:InputNumber("Input b2 for second line")
+        );
+}
+(double pointX, double pointY) GetIntersectionPoint((int, int, int, int) lineKoef){
+    // y = kx+b
+    // (b2 - b1) / (k1 - k2) = x
+    (int k1, int b1, int k2, int b2) = lineKoef;
+    double x, y;
+    x = (double) (b2 - b1) / (k1 - k2);
+    y = k1 * x + b1;
+    return (pointX: x, pointY: y);
+}
+var koef = InputKoefLine();
+var points = GetIntersectionPoint(koef);
+OutNumber(koef, points);
